a97e240fdd18a2e9212fb0ab89e7b301f487b4b4
sri hari <srihari4338@gmail.com>
2026-02-21 23:47:27 +0530

    added ok srihari

--- github-api-integration-module.sh
+++ github-api-integration-module.sh
@@ -5,7 +5,7 @@
 #
 #
 #
-# This script will help users to communicate and retrieve information from GitHub
+# This script will help users to communicate and retrieve information from GitHub ok srihari
 # Usage:
 #   Please provide your github token and rest api to the script as input
 #
